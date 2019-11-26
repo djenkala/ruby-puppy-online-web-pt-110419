981fb8397c7189e651bc5cc191a2509760efa7f3
Daniel Jenkala <djenkala@yahoo.com>
2019-11-26 21:32:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,6 @@ class Dog
   
   def initialize(name)
     @name = name
-    @@all << self
   end
   
   def self.all
@@ -14,6 +13,14 @@ class Dog
   
   def self.print_all
     puts @@all.map{ |dog| dog.name }
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.save
+    @@all << self
   end
   
 end
